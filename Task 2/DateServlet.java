2687f1ff813b72b93a49330da4069551fe479de4
Matheus Frigo <matheus.tfrigo@gmail.com>
2019-11-16 13:52:42 +0100

    Task 2

---
+++ Task 2/DateServlet.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import java.util.Date;
+
+public class DateServlet extends HttpServlet {
+	@Override
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
+	throws IOException, ServletException {
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		try {
+			out.println("<html>");
+
+			out.println("<head><title>Hello, world</title></head>");
+			out.println("<body>");
+			out.println("<h1>Hello, world!</h1>"); 
+			out.println("<h2>" + new Date() + "</h2>");
+			out.println("</body></html>");
+		} finally {
+			out.close();
+		}
+	}
+}
